4bad12efde48a0c9c588bd92c1248cce2dfd1e8c
Annak506 <annakleinloog@hotmail.com>
2024-08-23 12:45:16 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      Hello
-      </header>
-    </div>
-  );
+    <div className="container">
+    <h1 className="text-center mt-5">Testing Bootstrap in React</h1>
+    <button className="btn btn-primary">Bootstrap Button</button>
+  </div>
+);
 }
 
 export default App;

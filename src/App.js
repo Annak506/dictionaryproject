bcb74ee5d9f666784ca830730fc00cd9a1c34117
Annak506 <annakleinloog@hotmail.com>
2024-08-26 12:51:36 +0300

    set up the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dictionary from './Dictionary.js';
 
 function App() {
   return (
-    <div className="container">
-    <h1 className="text-center mt-5">Testing Bootstrap in React</h1>
-    <button className="btn btn-primary">Bootstrap Button</button>
+    <div className="App">
+      <div className="container">
+        <header className = "App-Header">
+          <h1 className="text-center mt-5">Testing Bootstrap in React</h1>
+          <button className="btn btn-primary">Bootstrap Button</button>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+
+        </footer>
+    </div>
   </div>
 );
 }

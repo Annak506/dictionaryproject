0fb6091989d0344b814aaa33588d18f537ad0d82
Annak506 <annakleinloog@hotmail.com>
2024-08-28 11:17:41 +0300

    added photos api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,12 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     
+        
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Anna</small>
+          <small>Coded by Anna Banneberg-Kleinloog</small>
         </footer>
       </div>
     </div>

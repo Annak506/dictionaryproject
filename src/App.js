d92df726afc41a79a045fecef5d408fadedc91fa
Annak506 <annakleinloog@hotmail.com>
2024-08-28 09:57:00 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header">
+
+        </header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Anna Bannenberg-Kleinloog</small>
+          <small>Coded by Anna</small>
         </footer>
       </div>
     </div>
